8cf2b7cb7675cf07a9d1a3bf3dc67809e5ef09da
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-05-06 19:36:27 +0530

    add bootstrap collapsable component

--- view orders/script.js
+++ view orders/script.js
@@ -1,9 +1,9 @@
 const DB_API = 'http://localhost:3300/get-orders';
 
-fetch(DB_API)
-.then(res=>{
-    return res.json();
-})
-.then(data => {
-    console.log(data);
-})
+// fetch(DB_API)
+// .then(res=>{
+//     return res.json();
+// })
+// .then(data => {
+//     console.log(data);
+// })
